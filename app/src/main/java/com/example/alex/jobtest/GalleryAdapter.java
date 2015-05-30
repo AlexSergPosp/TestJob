0c10ad694c2b77f1c25ca9eae28c9f7fc00f7e02
Alex <alex.serg.posp@gmail.com>
2015-05-30 21:26:06 +0600

    first commit

--- app/src/main/java/com/example/alex/jobtest/GalleryAdapter.java
+++ app/src/main/java/com/example/alex/jobtest/GalleryAdapter.java
@@ -15,28 +15,34 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 
 public class GalleryAdapter extends Fragment {
 
-    static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
-    int pageNumber;
+     static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+     static int pageNumber;
+     static String textForTextView;
      public static ImageView imageView;
      public static TextView textView;
      public static ProgressBar progressBar;
      DisplayImageOptions defaultOptions;
      ImageLoaderConfiguration config;
-
-    public static String[] URL1 = {"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRhG5rMM3UX8z4JG0PE9JnTPa7iaXOiB6pLE_soQNbwlryt8mdy5A",
-            "http://rewalls.com/large/201203/63548.jpg"
-            ,"http://www.nastol.com.ua/pic/201208/2560x1440/nastol.com.ua-29508.jpg"
-            ,"http://kdlt.ru/wp-content/uploads/2012/09/127.jpg"
-            ,"http://www.wallsgeneration.ru/images/original/lamba-[1920x1200]-[2385235].jpg"
-            ,"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR81wd_ZmOYeFtHCPPQBheBILa7pOWU0VZl3u2gdz7WsVp0Un-w"
-            ,"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR0TD8VTNwUGR5GpBwqGaPsYeWg4FMibzaO4vrhACKsWYDlgCKhfw"
-            ,"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR7kc-mLhgU28WVKduFfAjeVZPmUt1ybUjejT-hv_jkOH0bH3Ld"
-            ,"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSzeU-InYeIZbDjb8bxz5NzsR3QCcPtHhd9f_jaT2gYImoyYfFA"
-            ,"http://f9.ifotki.info/org/71a2543234450c4cd8b01c150cccfcc44e24d797267247.jpg"};
-
+    public static String URL1;
+            // Constructor
     static GalleryAdapter newInstance(int page){
             Log.d(MainActivity.TAG,"GalleryAdapter newInstance");
             GalleryAdapter galleryAdapter = new GalleryAdapter();
@@ -50,8 +56,14 @@ public class GalleryAdapter extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         Log.d(MainActivity.TAG, "GalleryAdapter onCreate");
         super.onCreate(savedInstanceState);
+
+        //get page number from constructor and use as adapter's arguments
+        if (MainActivity.prefRand){
+            Random random = new Random();
+            pageNumber = random.nextInt(MainActivity.pageCount);
+        }
         pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
-        // do rand or raw
+        //use Universal Loader(in dependencies)
         defaultOptions = new DisplayImageOptions.Builder()
                 .cacheInMemory(true)
                 .build();
@@ -59,16 +71,71 @@ public class GalleryAdapter extends Fragment {
                 .defaultDisplayImageOptions(defaultOptions)
                 .build();
         ImageLoader.getInstance().init(config);
+
+        // get Image list from getJsonData method
+        try {
+            HashMap<Integer,Image> arrayList = getJsonData();
+            URL1 = arrayList.get(pageNumber).getURL();
+            textForTextView = arrayList.get(pageNumber).getID();
+
+        } catch (IOException e) {
+            Log.d(MainActivity.TAG,"GalleryAdapter onCreate Exception");
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             Log.d(MainActivity.TAG, "GalleryAdapter onCreateView");
             View v = inflater.inflate(R.layout.gallery,null);
-
             imageView = (ImageView) v.findViewById(R.id.imageView);
             progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
             textView = (TextView) v.findViewById(R.id.textView);
-            ImageLoader.getInstance().displayImage(URL1[pageNumber],imageView);
+            ImageLoader.getInstance().displayImage(URL1, imageView);
+            textView.setText(textForTextView);
         return v;
+    }
+
+
+
+
+         // parsing Json from resource
+         String loadJsonFromRes() throws IOException {
+        Log.d(MainActivity.TAG,"start");
+        InputStream is = getResources().openRawResource(R.raw.json);
+        Log.d(MainActivity.TAG,"2");
+        Writer writer = new StringWriter();
+        Log.d(MainActivity.TAG,"3");
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } finally {
+            is.close();
+        }
+        Log.d(MainActivity.TAG, "4");
+        return writer.toString();
+    }
+        HashMap<Integer,Image> getJsonData() throws JSONException, IOException {
+        JSONObject jsonObject = new JSONObject(loadJsonFromRes());
+        JSONArray array = jsonObject.getJSONArray("image");
+        HashMap<Integer,Image> imageList = new HashMap<>();
+        for (int i = 0; i <array.length();i++) {
+                    JSONObject object = array.getJSONObject(i);
+                    int num = Integer.parseInt(object.getString("number"));
+                    Image image = new Image();
+                    Log.d(MainActivity.TAG, "5");
+                    image.setID(object.getString("id"));
+                    image.setNUMBER(object.getString("number"));
+                    image.setURL(object.getString("url"));
+                    imageList.put(num-1, image);
+        }
+        return imageList;
     }
 }
--- app/src/main/java/com/example/alex/jobtest/MainActivity.java
+++ app/src/main/java/com/example/alex/jobtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alex.jobtest;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.nfc.Tag;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -14,11 +16,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends FragmentActivity {
     public static String TAG = "mylog";
-    static final int pageCount = 5;
-
+    static final int pageCount = 10;
+    private Handler handler;
+    private Runnable runnable;
+    Timer timer;
+    int position;
     ViewPager viewPager;
     PagerAdapter pagerAdapter;
     SharedPreferences sp;
@@ -31,6 +39,7 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onResume() {
+        Log.d(TAG,"onResume");
         super.onResume();
         getPref();
     }
@@ -42,9 +51,11 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager) findViewById(R.id.pager);
         pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+       // viewPager.setPageTransformer(true, new DepthPageTransformer());
         viewPager.setAdapter(pagerAdapter);
         viewPager.setOnPageChangeListener(getListenerAdapter());
         sp = PreferenceManager.getDefaultSharedPreferences(this);
+
 
     }
 
@@ -97,17 +108,75 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+        if (handler!= null) {
+            handler.removeCallbacks(runnable);
+        }
+        if (timer != null){
+            timer.cancel();
+        }
+
+    }
+
+     // get and use them
     public void getPref(){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         prefAuto = preferences.getBoolean("auto",false);
         prefFavorite = preferences.getBoolean("favorite", false);
-        prefRand = preferences.getBoolean("random",false);
+        prefRand = preferences.getBoolean("random", false);
         prefInterval = preferences.getString("interval", "1");
         prefAnim = preferences.getString("list","1");
-        Log.d(MainActivity.TAG,"get pref(), prefAuto - " + prefAuto
-                +" prefFavorite - " + prefFavorite
-                +" prefRand - " + prefRand
-                +" prefInterval - " + prefInterval
-                +" prefAnim - " + prefAnim);
+        Log.d(MainActivity.TAG, "get pref(), prefAuto - " + prefAuto
+                + " prefFavorite - " + prefFavorite
+                + " prefRand - " + prefRand
+                + " prefInterval - " + prefInterval
+                + " prefAnim - " + prefAnim);
+        autoSliding();
+        animation(Integer.parseInt(prefAnim));
+
+
+    }
+    // if auto sliding turn on? then wse this method
+
+   private void autoSliding(){
+        if (prefAuto) {
+            Log.d(TAG, "if start");
+            handler = new Handler();
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    if (position > 4) {
+                        position = 0;
+                    } else {
+                        position++;
+                    }
+                    Log.d(TAG, "run start");
+                    viewPager.setCurrentItem(position, true);
+                }
+
+            };
+            int sec = Integer.parseInt(prefInterval) * 1000;
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(runnable);
+                }
+            }, 1000, sec);
+
+        }
+    }
+    private void animation(int i){
+        switch (i) {
+            case 1: viewPager.setPageTransformer(true, new DepthPageTransformer());
+                break;
+            case 2: viewPager.setPageTransformer(true, new ZoomOutPageTransforme());
+                break;
+            case 3: viewPager.setPageTransformer(true, new DepthPageTransformer());
+                break;
+        }
     }
 }
